05df080cf1836d0d1ec265ea934fdaaaa1864bc9
Ivan Admaers <ivanadmaers@MacBook-Pro.local>
2020-11-24 11:48:30 +0400

    update

--- src/js/main.js
+++ src/js/main.js
@@ -1,32 +1,107 @@
 'use strict';
-const year = 2021;
+//Page img
+const setPageImg = (imgSelector=>{
+  if(!imgSelector) return;
 
-const yearBlock = document.getElementById('year');
-yearBlock.textContent = year;
+  //Random arr elem
+  const randomElem = arr=>{
+    return arr[Math.floor(Math.random() * arr.length)];
+  };
 
-const countDate = new Date(`jan 1, ${year} 00:00:00`).getTime();
-
-const counter = ()=>{
-  const timeNow = new Date().getTime(),
-        gap = countDate - timeNow;
+  const imgArr = ['newyear1.webp', 'newyear2.webp', 'newyear3.webp', 'newyear4.webp', 'newyear5.webp'],
+        path = './src/img/';
+  const imgArea = document.querySelector(imgSelector);
   
-  const seconds = 1000,
-        minutes = seconds * 60,
-        hours = minutes * 60,
-        days = hours * 24;
+  imgArea.style.backgroundImage = `url("${path}${randomElem(imgArr)}")`;;
+})('.container .img');
 
-  const d = Math.floor(gap / days),
-        h = Math.floor((gap % days) / hours),
-        m = Math.floor((gap % hours) / minutes),
-        s = Math.floor((gap % minutes) / seconds);
+//Get next year
+const getNextYear = ()=>{
+  return new Promise(async (resolve, reject)=>{
+    
+    //Get year from some site
+    const getSiteHTML = async site=>{
+      return new Promise(async (resolve, reject)=>{
+        try{
+          const req = await fetch(site);
+          const res = req.ok ? await req.text() : reject(e);
+  
+          return resolve(res);
+        } catch(e){
+          reject(e);
+        }
+      });
+    };
 
-  document.getElementById('day').textContent = d;
-  document.getElementById('hour').textContent = h;
-  document.getElementById('minute').textContent = m;
-  document.getElementById('second').textContent = s;
+    const getYearFromHTML = (site, selector)=>{
+      return new Promise(async (resolve, reject)=>{
+        try{
+          const pageHTML = await getSiteHTML(site);
+          const parser = new DOMParser();
+          const doc = parser.parseFromString(pageHTML, 'text/html');
+          const year = doc.querySelector(selector).textContent.replace(/[^+\d]/g, '');
+          
+          return resolve(+year);
+        } catch(e){
+          reject(new Error(e));
+        }
+      });
+    };
+
+    //Calc next year
+    let year = 0;
+    try{
+      year = await getYearFromHTML('https://jsonplaceholder.typicode.com', 'body > footer > div:nth-child(2)');
+    } catch(e){
+      console.log(`Cannot get year from site. ${e}`);
+      year = new Date().getFullYear();
+    }
+
+    if(year){
+      return resolve(year + 1);
+    } else {
+      return reject('Something wrong');
+    }
+    
+  });
 };
 
-counter();
-setInterval(()=>counter(), 1000);
+(async ()=>{
+  let year = 0;
+  try{
+    year = +await getNextYear();
+  } catch(e){
+    console.log(`Something wrong. ${e}`);
+    year = new Date.getFullYear() + 1;
+  }
+
+  const yearBlock = document.getElementById('year');
+  yearBlock.textContent = year;
+
+  const countDate = new Date(`jan 1, ${year} 00:00:00`).getTime();
+
+  const counter = () => {
+    const timeNow = new Date().getTime(),
+      gap = countDate - timeNow;
+
+    const seconds = 1000,
+      minutes = seconds * 60,
+      hours = minutes * 60,
+      days = hours * 24;
+
+    const d = Math.floor(gap / days),
+      h = Math.floor((gap % days) / hours),
+      m = Math.floor((gap % hours) / minutes),
+      s = Math.floor((gap % minutes) / seconds);
+
+    document.getElementById('days').textContent = d;
+    document.getElementById('hours').textContent = h;
+    document.getElementById('minutes').textContent = m;
+    document.getElementById('seconds').textContent = s;
+  };
+
+  counter();
+  setInterval(() => counter(), 1000);
+})();
 
 console.log('%c Waiting...', 'padding: 20px 0;color:#fff;text-transform:capitalize;font-size:3em;');
